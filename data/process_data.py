330b9c8ec5be0d0f581fec58b12b2d8dff5fca68
lucas <lucasbaiao@gmail.com>
2021-09-08 09:59:20 -0300

    etl pipeline working and documented.

--- data/process_data.py
+++ data/process_data.py
@@ -1,19 +1,92 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Properly loads and preprocess the data.
 
+    Parameters
+    ---
+        messages_filepath: str
+            Messages csv file path.
+        categories_filepath: str
+            Categories csv file path.
+
+    Returns
+    ---
+    df: pandas.DataFrame
+        Dataframe containing the data, properly processed.
+    """
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    messages = messages.drop(columns='original')
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    # merge datasets
+    df = pd.merge(right=categories, left=messages, how='inner', on='id')
+    # create a dataframe of the 36 individual category columns
+    categories = categories['categories'].str.split(';', -1, True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x: x.split('-')[0])
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    # convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1])
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+    # drop the original categories column from `df`
+    df = df.drop(columns=['categories'])
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    return df
+    
 
 def clean_data(df):
-    pass
+    """Remove duplicates and NaN values.
+
+    Parameters
+    ---
+    df: pandas.DataFrame
+        Dataframe with the preprocessed data.
+
+    Returns
+    ---
+    df: pandas.DataFrame
+        Dataframe without duplicates and NaN values.
+    """
+    # drop duplicates
+    df = df.drop_duplicates()
+    # since i do not know the meaning of label "2", i will drop these rows, since they are just a few.  # noqa
+    df = df[df['related'] < 2]
+    # drop null values
+    df = df.dropna()
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """Loads the preprocessed data into a SQLite database.
+
+    Parameters
+    ---
+    df: pandas.DataFrame
+        Dataframe with the data the will be loaded into the database.
+    database_filename: str
+        SQLite database filename.
+    """
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('InsertTableName', engine, index=False)
 
 
 def main():
+    """Executes the ETL pipeline.
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
